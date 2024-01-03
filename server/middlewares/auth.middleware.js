a8f9a0b2a40bf85f3629ea68eb19d99232f66317
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-03 14:00:37 +0200

    bug-fix: Handling errors

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -2,36 +2,34 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({error: "Unauthorized"});
-  }
-
   try {
+    
+    const token = req.headers["authorization"]?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({error: "Unauthorized"});
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ username: decoded.username }).select(
+    const user = await User.findOne({ email: decoded.email }).select(
       "-password"
     );
-    
+
     if (!user) {
-      return res.status(404).send("User not found");
+      return res.status(404).json({error: "User not found"});
     }
 
     req.user = user;
     next();
   } catch (error) {
-    console.error("Token verification error:", error);
-
     if (error.name === "TokenExpiredError") {
-      return res.status(401).send("Token has expired");
+      return res.status(401).json({error: "Token has expired"});
     }
 
     if (error.name === "NotBeforeError") {
-      return res.status(401).send("Token not yet valid");
+      return res.status(401).json({error: "Token not yet valid"});
     }
 
-    return res.status(401).send("Invalid token");
+    return res.status(401).json({error: "Invalid token"});
   }
 };
 
