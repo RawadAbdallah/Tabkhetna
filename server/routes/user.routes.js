37dd33a45f36fab5cec4b6388bd3cde4bc44573f
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-03 14:45:28 +0200

    feat: Reject Cookmate route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { addCookmate, acceptCookmate } = require('../controllers/user.controller')
+const { addCookmate, acceptCookmate, rejectCookmate } = require('../controllers/user.controller')
 const router = express.Router()
 
 router.post('/add/:id', addCookmate)
 router.post('/accept/:id', acceptCookmate)
+router.post('/reject/:id', rejectCookmate)
+
 
 module.exports = router
