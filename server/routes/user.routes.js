a37fe3c316ecc5a5eaf9b72745f374dc03fb30e0
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-04 13:10:28 +0200

    feat(core): Adding getCookmate route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { addCookmate, acceptCookmate, rejectCookmate } = require('../controllers/user.controller')
+const { addCookmate, acceptCookmate, rejectCookmate, getCookmates } = require('../controllers/user.controller')
 const router = express.Router()
 
 router.post('/add/:id', addCookmate)
 router.post('/accept/:id', acceptCookmate)
 router.post('/reject/:id', rejectCookmate)
-
+router.get('/', getCookmates) // this is giving me an error  Route.get() requires a callback function but got a [object Undefined]
 
 module.exports = router
