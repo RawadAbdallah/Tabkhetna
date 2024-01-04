943b4c9d7dc1b9f8fa1e75112939d7e3363e50c2
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-04 13:11:48 +0200

    refactor: removing comment

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,6 +5,6 @@ const router = express.Router()
 router.post('/add/:id', addCookmate)
 router.post('/accept/:id', acceptCookmate)
 router.post('/reject/:id', rejectCookmate)
-router.get('/', getCookmates) // this is giving me an error  Route.get() requires a callback function but got a [object Undefined]
+router.get('/', getCookmates)
 
 module.exports = router
