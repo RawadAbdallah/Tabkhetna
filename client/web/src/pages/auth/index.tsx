2b35146a77cb5430e9eb916598122d5fbe8f2e6b
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-11 22:59:38 +0200

    refactor: Created a function for valditing email input

--- client/web/src/pages/auth/index.tsx
+++ client/web/src/pages/auth/index.tsx
@@ -127,6 +127,17 @@ const Auth: React.FC = () => {
         }
     };
 
+    const validateEmail = (email:string):string => {
+        if(!email){
+          return "Email is missing"
+        }
+      
+        const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!emailRegex.test(email)){
+          return "Email format is invalid"
+        }
+        return ""
+      }
     const validateForm = () => {
         const { email, firstname, lastname, password, confirm_password } =
             credentials;
