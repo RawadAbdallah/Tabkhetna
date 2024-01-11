b08351681afd1eb2ad464656b403a68da8be3a73
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-12 00:02:25 +0200

    chore: Created credentials types

---
+++ client/web/src/types/credentials.ts
@@ -0,0 +1,9 @@
+export type Credentials = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+  profile_pic?: string | null;
+  keep_me_logged_in?: boolean;
+};
