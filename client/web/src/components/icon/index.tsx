3c9b10135276ce5de030cbd26c663ac3de9ecda2
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:08:36 +0200

    [front-end] feat(component): Added noBackground prop where it decides if the component has a background or not

--- client/web/src/components/icon/index.tsx
+++ client/web/src/components/icon/index.tsx
@@ -3,11 +3,12 @@ import "./icon.css";
 type IconProps = {
     img: string;
     alt: string;
+    noBackground?: boolean;
 };
-const Icon: React.FC<IconProps> = (props) => {
+const Icon: React.FC<IconProps> = ({img, alt, noBackground = false}) => {
     return (
-        <div className="icon">
-            <img src={props.img} alt={props.alt} />
+        <div className={noBackground ? "icon transparent-icon" : "icon"}>
+            <img src={img} alt={alt} />
         </div>
     );
 };
