50f13c526d5db5eda45631ab15de1c42c6d28fd0
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 03:04:20 +0200

    [front-end] refactor: Applied Prettier

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,23 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Auth from "./pages/auth"
-import Home from "./pages/home"
+import Auth from "./pages/auth";
+import Home from "./pages/home";
 
-
-import './styles/reset.css'
-import './styles/global.css'
+import "./styles/reset.css";
+import "./styles/global.css";
 
 function App() {
-
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth" Component={Auth} />
-        <Route path="/" Component={Home} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth" Component={Auth} />
+                <Route path="/" Component={Home} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
