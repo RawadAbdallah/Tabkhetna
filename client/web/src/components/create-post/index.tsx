c9a9e558c106e8e2b56f21cf65c0070f04ddbc0f
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:06:23 +0200

    [front-end] feat(component): Created new CreatePost component

---
+++ client/web/src/components/create-post/index.tsx
@@ -0,0 +1,60 @@
+import Icon from "../icon";
+
+import "./create-post.css";
+
+import default_profile_pic from "../../assets/images/default_profile_pic.png";
+import book_icon from "../../assets/images/book_dark_icon.svg";
+import camera_icon from "../../assets/images/camera_icon.svg";
+import video_icon from "../../assets/images/video_icon.svg";
+import challenge_icon from "../../assets/images/challenge_dark_icon.svg";
+
+const CreatePost: React.FC = () => {
+    return (
+        <div className="create-post-container flex flex-column gap-5">
+            <div className="flex gap-3">
+                <img src={default_profile_pic} alt="Profile Pic" />
+                <input
+                    type="text"
+                    placeholder="What are we cooking today?"
+                    id="new-post"
+                    name="new-post"
+                />
+            </div>
+
+            <div className="create-post-types-container">
+                <ul className="post-types flex justify-between">
+                    <li className="flex align-center gap-3">
+                        <Icon
+                            img={camera_icon}
+                            alt="camera"
+                            noBackground={true}
+                        />
+                        Go Live
+                    </li>
+                    <li className="flex align-center gap-3">
+                        <Icon img={book_icon} alt="book" noBackground={true} />
+                        Recipe
+                    </li>
+                    <li className="flex align-center gap-3">
+                        <Icon
+                            img={video_icon}
+                            alt="video"
+                            noBackground={true}
+                        />
+                        Video
+                    </li>
+                    <li className="flex align-center gap-3">
+                        <Icon
+                            img={challenge_icon}
+                            alt="challenges"
+                            noBackground={true}
+                        />
+                        Challenge
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default CreatePost;
