ab7f55c86fd04657a1d208c8ae541e90f475dbe1
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:21:36 +0200

    [front-end] chore: Moved error component folder from pages to here

---
+++ client/web/src/components/error/index.tsx
@@ -0,0 +1,25 @@
+import "../error.css";
+import Header from "../header";
+import { goBack } from "../../utils/helper";
+
+type errorProps = {
+    statusCode: number;
+    errorMessage: string;
+};
+
+const Error: React.FC<errorProps> = ({ statusCode, errorMessage }) => {
+    return (
+        <>
+            <div className=" flex flex-column">
+                <Header />
+                <div className=" error-container flex flex-column align-center justify-center">
+                    <h1>Error {statusCode}</h1>
+                    <p>{errorMessage}</p>
+                    <button onClick={goBack}>Go Back</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Error;
