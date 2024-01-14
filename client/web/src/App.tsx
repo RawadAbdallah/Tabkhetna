7e6dbe1125bc35f6212c4b5384f97d0896358e27
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:36:57 +0200

    [front-end] refactor: Reorganized the usage of Error component

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/auth";
 import Home from "./pages/home";
 
+import Error from "./components/error";
+
 import "./styles/reset.css";
 import "./styles/global.css";
 
@@ -10,8 +12,19 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/auth" Component={Auth} />
-                <Route path="/" Component={Home} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/" element={<Home />} />
+                {/* On the next line it is causing an error, since the Error element is expecting to pass props, but I can't pass props in the Component attribute */}
+                <Route
+                    path="*"
+                    element={
+                        <Error
+                            statusCode={400}
+                            errorMessage="Oh no! It seems that someone ate this page. No worries,
+                        we are here to guide you back to the feast."
+                        />
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
