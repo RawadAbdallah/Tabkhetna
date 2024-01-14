a896546269d6c83aa80c73bd0e385d64496f124f
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 02:48:13 +0200

    [front-end] refactor

--- client/web/src/pages/home/index.tsx
+++ client/web/src/pages/home/index.tsx
@@ -1,10 +1,14 @@
+import Sidebar from "../../components/sidebar";
 import Header from "../../components/header";
 import "./home.css";
 const Home: React.FC = () => {
     return (
-        <>
+        <div className="home-page">
             <Header />
-        </>
+            <div className="home-main flex align-items-left">
+                <Sidebar />
+            </div>
+        </div>
     );
 };
 
