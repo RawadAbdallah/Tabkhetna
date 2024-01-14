c7e160bc226e9e68adab307b73ef6f28afbe2456
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:12:06 +0200

    [front-end] feat(component): Added main section

--- client/web/src/pages/home/index.tsx
+++ client/web/src/pages/home/index.tsx
@@ -1,12 +1,19 @@
 import Sidebar from "../../components/sidebar";
 import Header from "../../components/header";
+import CreatePost from "../../components/create-post";
 import "./home.css";
 const Home: React.FC = () => {
     return (
         <div className="home-page">
             <Header />
-            <div className="home-main flex align-items-left">
+            <div className="home-main flex">
                 <Sidebar />
+                <section className="main-section">
+                    <CreatePost/>
+                    <div className="posts-container">
+                        
+                    </div>
+                </section>
             </div>
         </div>
     );
