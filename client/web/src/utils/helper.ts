95b3001bdc0c1e7c2c19e48987268983a48488e8
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-14 09:25:11 +0200

    [front-end] chore: Added new helper function

--- client/web/src/utils/helper.ts
+++ client/web/src/utils/helper.ts
@@ -1,60 +1,81 @@
 //Auth Page helper functions
 import Credentials from "../types/credentials";
 
-export const validateEmail = (email:string):string => {
-  if(!email){
-    return "Email is missing"
-  }
+export const validateEmail = (email: string): string => {
+    if (!email) {
+        return "Email is missing";
+    }
 
-  const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(!emailRegex.test(email)){
-    return "Email format is invalid"
-  }
-  return ""
-}
+    const emailRegex: RegExp =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+        return "Email format is invalid";
+    }
+    return "";
+};
 
-export const validateForm = (credentials:Credentials,  setIsInvalid: React.Dispatch<React.SetStateAction<Credentials>>) => {
-  const { email, firstname, lastname, password, confirm_password } =
-      credentials;
-  //email validation
-  const emailError = validateEmail(email)
-  if(emailError){
-      setIsInvalid((prev) => ({...prev, email: emailError}) )
-  }
+export const validateForm = (
+    credentials: Credentials,
+    setIsInvalid: React.Dispatch<React.SetStateAction<Credentials>>
+) => {
+    const { email, firstname, lastname, password, confirm_password } =
+        credentials;
+    //email validation
+    const emailError = validateEmail(email);
+    if (emailError) {
+        setIsInvalid((prev) => ({ ...prev, email: emailError }));
+    }
 
-  //password validation
-  if(!password){
-      setIsInvalid((prev) => ({...prev, password: "Password is missing"}) )
-  }
+    //password validation
+    if (!password) {
+        setIsInvalid((prev) => ({ ...prev, password: "Password is missing" }));
+    } else if (password.length < 6 || password.length > 16) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            password: "Password should be between 6 and 16 characters",
+        }));
+    }
 
-  else if(password.length < 6 || password.length > 16){
-      setIsInvalid((prev) => ({...prev, password: "Password should be between 6 and 16 characters"}) )
-  }
+    //confirm password validation
+    if (!confirm_password) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            confirm_password: "Confirm password is missing",
+        }));
+    } else if (confirm_password !== password) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            confirm_password: "Passwords don't match",
+        }));
+    }
 
-  //confirm password validation
-  if(!confirm_password){
-      setIsInvalid((prev) => ({...prev, confirm_password: "Confirm password is missing"}))
-  }
-  else if(confirm_password !== password){
-      setIsInvalid((prev) => ({...prev, confirm_password: "Passwords don't match"}))
-  }
+    //Firstname validation
 
-  //Firstname validation
+    if (!firstname) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            firstname: "Firstname is missing",
+        }));
+    } else if (firstname.length <= 2) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            firstname: "Firstname should be at least 2 characters",
+        }));
+    }
 
-  if(!firstname){
-      setIsInvalid((prev) => ({...prev, firstname: "Firstname is missing"}))
-  }
+    // Lastname validation
+    if (!lastname) {
+        setIsInvalid((prev) => ({ ...prev, lastname: "Lastname is missing" }));
+    } else if (lastname.length <= 2) {
+        setIsInvalid((prev) => ({
+            ...prev,
+            lastname: "Lastname should be at least 2 characters",
+        }));
+    }
+};
 
-  else if(firstname.length <= 2){
-      setIsInvalid((prev) => ({...prev, firstname: "Firstname should be at least 2 characters"}))
-  }
+// Go back feature function
 
-  // Lastname validation
-  if(!lastname){
-      setIsInvalid((prev) => ({...prev, lastname: "Lastname is missing"}))
-  }
-
-  else if(lastname.length <= 2){
-      setIsInvalid((prev) => ({...prev, lastname: "Lastname should be at least 2 characters"}))
-  }
+export const goBack = () => {
+    window.history.back();
 };
