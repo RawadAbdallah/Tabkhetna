f1b357595ecb07f46388d18d8904bf07d99db8d8
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-16 02:53:55 +0200

    [front-end] feat(component): Passed current page state as a prop

--- client/web/src/components/sidebar/index.tsx
+++ client/web/src/components/sidebar/index.tsx
@@ -10,36 +10,39 @@ import calendar_icon from "@images/calendar_icon.svg";
 
 import "./sidebar.css";
 
+type SidebarPropType = {
+    current_page?: string,
+}
 
-const Sidebar: React.FC = () => {
+const Sidebar: React.FC<SidebarPropType> = ({current_page}) => {
     return (
         <div className="sidebar">
             <ul className="sidebar-items">
-                <li className="sidebar-item active">
+                <li className={`sidebar-item ${current_page && current_page === "home" ? "active" : ""}`}>
                     <Link to="/" className="sidebar-link">
                         <Icon img={home_icon} alt="home" />
                         Home
                     </Link>
                 </li>
-                <li className="sidebar-item">
+                <li className={`sidebar-item ${current_page && current_page === "cookmates" ? "active" : ""}`}>
                     <Link to="/cookmates" className="sidebar-link">
                         <Icon img={cookmates_icon} alt="cookmates" />
                         Cookmates
                     </Link>
                 </li>
-                <li className="sidebar-item">
+                <li className={`sidebar-item ${current_page && current_page === "recipes" ? "active" : ""}`}>
                     <Link to="/recipes/saved" className="sidebar-link">
                         <Icon img={book_icon} alt="recipes" />
                         Saved Recipes
                     </Link>
                 </li>
-                <li className="sidebar-item">
+                <li className={`sidebar-item ${current_page && current_page === "challenges" ? "active" : ""}`}>
                     <Link to="/challenges" className="sidebar-link">
                         <Icon img={challenge_icon} alt="challenges" />
                         Challenges
                     </Link>
                 </li>
-                <li className="sidebar-item">
+                <li className={`sidebar-item ${!current_page && current_page === "events" ? "active" : ""}`}>
                     <Link to="/profile" className="sidebar-link">
                         <Icon img={calendar_icon} alt="events" />
                         Events
@@ -48,7 +51,7 @@ const Sidebar: React.FC = () => {
 
                 <div className="line-seperator"></div>
 
-                <li className="sidebar-item">
+                <li className={`sidebar-item ${current_page && current_page === "support" ? "active" : ""}`}>
                     <Link to="/support" className="sidebar-link">
                         <Icon img={question_icon} alt="?" />
                         Support
