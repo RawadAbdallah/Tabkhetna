3c46707436779442dde811383ab933d146a5feee
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-16 23:10:02 +0200

    [front-end] refactor: Changed last_online type

---
+++ client/web/src/types/cookmate.ts
@@ -0,0 +1,10 @@
+type Cookmate = {
+  id: string,
+  profile_pic: string,
+  firstname: string,
+  lastname: string,
+  is_online: boolean,
+  last_online?: string | undefined
+}
+
+export default Cookmate
