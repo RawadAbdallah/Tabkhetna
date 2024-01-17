26248094e45095a30d4942a54a6e84c1e8ed2b8d
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-18 00:56:03 +0200

    [back-end] feat(model): Configured the comments in the post model

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -66,10 +66,7 @@ const postSchema = new mongoose.Schema(
                 user: {
                     type: mongoose.Schema.Types.ObjectId,
                     ref: "User",
-                },
-                comment_image: {
-                    type: String,
-                },
+                }
             },
         ],
     },
