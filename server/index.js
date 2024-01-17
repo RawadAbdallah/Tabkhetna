49e8cb622005390949472af37f5fb2512a6c70b6
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-18 00:52:38 +0200

    [back-end] feat(server): Changed the router base url

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 const userRoutes = require('./routes/user.routes');
-app.use('/cookmates', authMiddleware, userRoutes)
+app.use('/api/cookmates', authMiddleware, userRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Server listening on port: ", process.env.PORT);
