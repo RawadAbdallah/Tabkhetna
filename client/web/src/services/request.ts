24ee3eed0976af1c57a1c923ce47e3006ee9fb89
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-18 00:53:43 +0200

    [front-end] feat(api): Configured the error handling

--- client/web/src/services/request.ts
+++ client/web/src/services/request.ts
@@ -26,7 +26,20 @@ export const request = async ({
 
         return response;
     } catch (error) {
-        const axiosError = error as AxiosError;
-        console.log("Error in API Request:", axiosError.message);
+        if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError;
+            if (axiosError.response) {
+                // There is a response from the server
+                const { status, data } = axiosError.response;
+                console.log(`Error in API Request - Status: ${status}, Message: ${data}`);
+                return axiosError.response;
+            } else {
+                // The error is not a server response error
+                console.log("Error in API Request:", axiosError.message);
+            }
+        } else {
+            // The error is not an Axios error
+            console.log("Non-Axios Error in API Request:", error);
+        }
     }
 };
