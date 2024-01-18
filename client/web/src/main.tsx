757eb5beabd58dfd28201da6cef1984891da2159
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-18 04:45:33 +0200

    [front-end] feat(redux): Wrapped the whole app with the provider

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store.tsx";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
