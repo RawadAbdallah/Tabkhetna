fa89c1ba686560506a65ce69eb770b6539c39912
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-18 02:42:47 +0200

    [front-end] feat(component): Added image to the Loader component

---
+++ client/web/src/components/loader/index.tsx
@@ -0,0 +1,11 @@
+import "./loader.css";
+
+const Loader: React.FC = () => {
+    return (
+        <div className="loader-wrapper">
+            <img src="/logo.png" alt="Tabkhetna" />
+        </div>
+    );
+};
+
+export default Loader;
