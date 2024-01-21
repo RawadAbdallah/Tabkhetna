bb2f1ca0e535688a5be2da33674dd391b00ddeec
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 03:54:32 +0200

    [front-end] feat(auth): Added additional security layer - Passed normal page component to a withAuthentication() HOC - Added a new Route for the Unauthorized page - Modified the 404 error message

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,34 +1,56 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// importing pages
 import Auth from "@pages/auth";
 import Home from "@pages/home";
-import Cookmates from '@pages/cookmates'
+import Cookmates from "@pages/cookmates";
 import Profile from "@pages/profile";
 
 import Error from "@components/error";
 
+// importing styles
 import "@styles/reset.css";
 import "@styles/global.css";
-import AdminPage from "./pages/admin";
+import AdminPage from "@pages/admin";
+
+import withAuthentication from "@utils/withAuthentication";
+
+// passing pages to the authentication HOC
+const AuthenticatedHome = withAuthentication(Home);
+const AuthenticatedCookmates = withAuthentication(Cookmates);
+const AuthenticatedProfile = withAuthentication(Profile);
+const AuthenticatedAdminPage = withAuthentication(AdminPage);
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
                 <Route path="/auth" element={<Auth />} />
-                <Route path="/cookmates" element={<Cookmates />} />
-                <Route path="/profile" element={<Profile />}/>
-                <Route path="/admin" element={<AdminPage/>} />
+                <Route path="/" element={<AuthenticatedHome />} />
+                <Route path="/cookmates" element={<AuthenticatedCookmates />} />
+                <Route path="/profile" element={<AuthenticatedProfile />} />
+                <Route path="/admin" element={<AuthenticatedAdminPage />} />
+                <Route
+                    path="/unauthorized"
+                    element={
+                        <Error
+                            statusCode={401}
+                            errorMessage="It seems you are not logged in. Redirecting you to Login page."
+                            errorTitle="Unauthorized"
+                            showGoBack={false}
+                            redirection={true}
+                        />
+                    }
+                />
                 <Route
                     path="*"
                     element={
                         <Error
                             statusCode={404}
+                            errorTitle="Page not found"
                             errorMessage="Oh no! It seems that someone ate this page. No worries,
                         we are here to guide you back to the feast."
                         />
-                        
                     }
                 />
             </Routes>
