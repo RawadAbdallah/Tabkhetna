572c3cc10e67b9e7f1e9d2650f24fef5942b5dc1
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 22:08:14 +0200

    [front-end] feat(component): Linked profile page to the backend apis

--- client/web/src/pages/profile/index.tsx
+++ client/web/src/pages/profile/index.tsx
@@ -2,8 +2,15 @@ import "./profile.css";
 import Header from "@components/header";
 import Sidebar from "@components/sidebar";
 import Post from "@components/post";
-
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { useEffect, useState } from "react";
+import { request, serverURL } from "@/services/request";
+import Loader from "@/components/loader";
+import default_profile_pic from "@/assets/images/default_profile_pic.png";
+import UserType from "@/types/user";
+import CookmateType from "@/types/cookmate";
 
 const data = {
     id: 1,
@@ -54,7 +61,12 @@ const data = {
             ],
         },
     ],
-    achievements: ['cookester', 'event master', 'challenger', 'recipe streaker'],
+    achievements: [
+        "cookester",
+        "event master",
+        "challenger",
+        "recipe streaker",
+    ],
     cookmates: [
         {
             id: "1",
@@ -119,31 +131,117 @@ const data = {
 };
 
 const Profile: React.FC = () => {
+    const user = useSelector((state: RootState) => {
+        return state.user;
+    });
+    const [profileData, setProfileData] = useState<UserType>({
+        _id: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        country: null,
+        role: 0,
+        is_online: false,
+        posts: [],
+        profile_pic: "",
+        achievements: [],
+    });
+    const [topCookmates, setTopCookmates] = useState<CookmateType[]>([
+        {
+            _id: "",
+            firstname: "",
+            lastname: "",
+            profile_pic: "",
+            is_online: false,
+        },
+    ]);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const getProfileData = async () => {
+        try {
+            if (user && user.token) {
+                setIsLoading(true);
+                // Fetch basic profile information first
+                const { token } = user;
+                const result = await request({
+                    route: "/profile/",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                });
+                setProfileData(result?.data || []);
+                setIsLoading(false);
+            }
+        } catch (e) {
+            console.log(e);
+            setIsLoading(false);
+        }
+    };
+
+    const getTopCookmates = async () => {
+        try {
+            if (user && user.token) {
+                setIsLoading(true);
+                console.log("getting top cookmates");
+                const { token } = user;
+                const result = await request({
+                    route: "/cookmates/top",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                });
+                setTopCookmates(result?.data.cookmates);
+                console.log("Top cookmates", topCookmates);
+                setIsLoading(false);
+            }
+        } catch (e) {
+            console.log(e);
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getProfileData();
+        getTopCookmates();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
+
     return (
         <div className="profile-page">
+            {isLoading && <Loader />}
             <Header />
             <main className="home-main flex">
                 <Sidebar />
                 <section className="profile-section flex flex-column gap-5">
                     <div className="profile-info-wrapper flex flex-column gap-3">
-                        <img className="profile-pic" src={data.profile_pic} />
+                        <img
+                            className="profile-pic"
+                            src={
+                                `${serverURL}uploads/images/${profileData.profile_pic}` ||
+                                default_profile_pic
+                            }
+                        />
                         <h2>
-                            {data.firstname} {data.lastname}
+                            {profileData.firstname} {profileData.lastname}
                         </h2>
                     </div>
 
                     <div className="achievements-wrapper flex align-center gap-5">
                         <div className="flex flex-column align-center">
                             <img
+                                loading="lazy"
                                 src={"src/assets/images/star_icon.svg"}
                                 alt="⭐"
                             />
                             <h2>Achievements</h2>
                         </div>
-                        {data.achievements.length > 0 ? (
+                        {profileData.achievements &&
+                        profileData.achievements.length > 0 ? (
                             <ul className="achievements-list">
-                                {data.achievements.map((item) => {
-                                    return <li>{item}</li>;
+                                {data.achievements.map((achievement, index) => {
+                                    return <li key={index}>{achievement}</li>;
                                 })}
                             </ul>
                         ) : (
@@ -153,48 +251,55 @@ const Profile: React.FC = () => {
 
                     <div className="profile-main gap-5">
                         <div className="posts-container flex flex-column gap-5">
-                            {data.posts.map((post, i) => {
-                                return (
-                                    <Post
-                                        key={i}
-                                        title={post.title}
-                                        uploader={post.uploader}
-                                        profile_pic={post.profile_pic}
-                                        created_at={post.created_at}
-                                        media={post.media}
-                                        likes={post.likes}
-                                        saves={post.saves}
-                                        comments={post.comments}
-                                        ingredients={post.ingredients}
-                                        instructions={post.instructions}
-                                    />
-                                );
-                            })}
+                            {profileData.posts &&
+                            profileData.posts.length > 0 ? (
+                                profileData.posts.map((post) => {
+                                    return (
+                                        <Post
+                                            key={post.title + post.uploader}
+                                            title={post.title}
+                                            uploader={post.uploader}
+                                            profile_pic={post.profile_pic}
+                                            created_at={post.created_at}
+                                            media={post.media}
+                                            likes={post.likes}
+                                            saves={post.saves}
+                                            comments={post.comments}
+                                            ingredients={post.ingredients}
+                                            instructions={post.instructions}
+                                        />
+                                    );
+                                })
+                            ) : (
+                                <div className="no-posts-headline">
+                                    Haven't Posted Yet
+                                </div>
+                            )}
                         </div>
                         <div className="profile-cookmates-wrapper flex flex-column gap-5">
                             <h2>
-                                {data.firstname} {data.lastname}'s Cookmates
+                                {profileData.firstname} {profileData.lastname}'s
+                                Cookmates
                             </h2>
                             <div className="cookmates-number flex align-center gap-3">
                                 <img
                                     src="/src/assets/images/cookmates_icon.svg"
                                     alt="cookmates icon"
                                 />
-                                <p>{data.cookmates.length} cookmates</p>
+                                <p>{topCookmates.length} cookmate(s)</p>
                             </div>
-                            {data.cookmates.length > 0 ? (
+                            {topCookmates && topCookmates.length > 0 ? (
                                 <ul className="profile-cookmates-list">
-                                    {data.cookmates.map((cookmate, index) => {
+                                    {topCookmates.map((cookmate) => {
                                         return (
-                                            <li key={index}>
+                                            <li key={cookmate._id}>
                                                 <Link
-                                                    to={`/user/${cookmate.id}`}
+                                                    to={`/user/${cookmate._id}`}
                                                     className="flex align-center"
                                                 >
                                                     <img
-                                                        src={
-                                                            cookmate.profile_pic
-                                                        }
+                                                        loading="lazy"
+                                                        src={`${serverURL}uploads/images/${cookmate.profile_pic}`}
                                                         alt="pic"
                                                     />
                                                     {cookmate.firstname}{" "}
@@ -207,7 +312,7 @@ const Profile: React.FC = () => {
                             ) : (
                                 <p> No cookmates yet!</p>
                             )}
-                            <Link to={`/profile/${data.id}/cookmates`}>
+                            <Link to={`/cookmates`}>
                                 See All {">"}
                             </Link>
                         </div>
