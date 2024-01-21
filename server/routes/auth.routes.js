b8a86809e0993690c6c36a61e4abdefe13685f7a
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 04:09:00 +0200

    [back-end] feat(routes): Passed multer middleware for the register route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { login, register } = require("../controllers/auth.controller");
+const upload = require("../configs/multer.config");
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/register", upload.single('profile_pic'), register);
 
 module.exports = router;
