89cd678d30fb3ce4d7f695fd00001838b0551105
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-22 01:05:14 +0200

    [back-end] feat(route): Create challenge route

---
+++ server/routes/challenge.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { getChallenges, createChallenge, participateChallenge } = require("../controllers/challenge.controller");
+const router = express.Router()
+
+router.get('/', getChallenges)
+router.post('/new', createChallenge)
+router.post('/participate/:challengeId', participateChallenge)
+
+module.exports = router;
