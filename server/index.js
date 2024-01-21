a1d1d648c7d6dad0446f0dfd9de16532b2eb6b35
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 22:28:38 +0200

    [back-end] feat(core): Required path and handled /uploads route to view saved files

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { authMiddleware } = require("./middlewares/auth.middleware");
 const { connectToMongoDB } = require("./configs/mongodb.config");
 const upload = require("./configs/multer.config");
 const bodyParser = require("body-parser");
-
+const path = require('path')
 require("dotenv").config();
 
 const app = express();
@@ -16,9 +16,9 @@ app.use(cors());
 app.options('*', cors)
 const authRoutes = require("./routes/auth.routes");
 app.use("/api/auth", authRoutes);
-
+app.use('/uploads', express.static(path.join(__dirname, 'storage/uploads')));
 const userRoutes = require('./routes/user.routes');
-app.use('/api/cookmates', authMiddleware, userRoutes)
+app.use('/api/', authMiddleware, userRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Server listening on port: ", process.env.PORT);
