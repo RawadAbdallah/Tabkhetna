06308115f90df999c2097983ac09138f7d64d4dd
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 04:11:10 +0200

    [back-end] feat(core): Increased the json and urlencoded size limit

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const { authMiddleware } = require("./middlewares/auth.middleware");
 const { connectToMongoDB } = require("./configs/mongodb.config");
 const upload = require("./configs/multer.config");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.options('*', cors)
 const authRoutes = require("./routes/auth.routes");
