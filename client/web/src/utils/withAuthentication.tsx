dae9438c09d5e874203fc389750a7a64642dc0d2
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 22:22:43 +0200

    [front-end] feat(HOC): Modifed the dispatch to pass id and the email along with the userSlice

--- client/web/src/utils/withAuthentication.tsx
+++ client/web/src/utils/withAuthentication.tsx
@@ -1,20 +1,40 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { checkAuthentication } from '@utils/helpers';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { checkAuthentication, getUser } from "@utils/helpers";
+import { useDispatch } from "react-redux";
+import { setUser } from "@/redux/userSlice";
 
 const withAuthentication = (WrappedComponent: React.FC) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (props: any) => {
-    const navigate = useNavigate();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (props: any) => {
+        const navigate = useNavigate();
+        const dispatch = useDispatch();
 
-    useEffect(() => {
-      if (!checkAuthentication()) {
-        navigate('/unauthorized')
-      }
-    }, [navigate]);
+        useEffect(() => {
+            if (!checkAuthentication()) {
+                navigate("/unauthorized");
+            } else {
+              // Storing user in the store
+                const user = getUser()
+                if (user !== null) {
+                    const parsedUser = JSON.parse(user);
+                    const token = parsedUser.token;
+                    dispatch(
+                        setUser({
+                            id: parsedUser.id,
+                            email: parsedUser.email,
+                            firstname: parsedUser.firstname,
+                            lastname: parsedUser.lastname,
+                            profile_pic: parsedUser.profile_pic,
+                            token: token,
+                        })
+                    );
+                }
+            }
+        }, [dispatch, navigate]);
 
-    return <WrappedComponent {...props} />;
-  };
+        return <WrappedComponent {...props} />;
+    };
 };
 
 export default withAuthentication;
