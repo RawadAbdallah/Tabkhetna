c3db866e0660f6bb2ae21a4a5995c5f4f358f871
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-21 04:04:07 +0200

    [front-end] feat(component): Added more props to pass - errorTitle prop to showcase the error title - redirection prop for the browser to redirect after countdown - showGoBack prop for rendering the go back button

--- client/web/src/components/error/index.tsx
+++ client/web/src/components/error/index.tsx
@@ -1,21 +1,54 @@
 import "./error.css";
-import Header from "@components/header";
-import { goBack } from "@utils/helper";
+import { goBack } from "@/utils/helpers";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 type errorProps = {
     statusCode: number;
     errorMessage: string;
+    errorTitle?: string;
+    showGoBack?: boolean;
+    redirection?: boolean;
 };
 
-const Error: React.FC<errorProps> = ({ statusCode, errorMessage }) => {
+const Error: React.FC<errorProps> = ({
+    statusCode,
+    errorTitle,
+    errorMessage,
+    showGoBack = true,
+    redirection = false,
+}) => {
+    const navigate = useNavigate()
+    const [countdown, setCountdown] = useState(5)
+
+    const startCountDown = () => {
+        setInterval(()=>{
+            setCountdown(countdown- 1)
+        }, 1000)
+    }
+
+    useEffect(()=>{
+        if(countdown < 0)
+            navigate('/auth')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [countdown])
+
     return (
         <>
             <div className=" flex flex-column">
-                <Header />
                 <div className=" error-container flex flex-column align-center justify-center gap-5">
                     <h1>Error {statusCode}</h1>
+                    <h1 className="error-title">
+                        {errorTitle}
+                    </h1>
                     <p>{errorMessage}</p>
-                    <button onClick={goBack}>Go Back</button>
+                    {showGoBack && <button onClick={goBack}>Go Back</button>}
+                    {redirection && (
+                        <>
+                            {startCountDown()}
+                            <h2>Redirecting you to Login page in <span>{countdown}</span></h2>
+                        </>
+                    )}
                 </div>
             </div>
         </>
