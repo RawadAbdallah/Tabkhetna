f5d2cee678dbbb037c0e083e983b6e1243f5f591
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-23 10:25:50 +0200

    [back-end] feat(route): Added top cookmates get route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -14,7 +14,7 @@ router.post("/cookmates/add/:id", addCookmate);
 router.post("/cookmates/accept/:id", acceptCookmate);
 router.post("/cookmates/reject/:id", rejectCookmate);
 router.get("/cookmates", getCookmates);
-router.get('/cookmates/top', getTopCookmates)
+router.get('/cookmates/top/:id', getTopCookmates)
 router.get("/profile", getCurrentUserProfile);
 router.get("/profile/:userId", getOtherUserProfile);
 
