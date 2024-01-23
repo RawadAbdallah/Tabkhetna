beb2c9cf3cc7502afafa37af1e41c8a58b536ac1
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-23 23:54:16 +0200

    [front-end] feat(component): Added major post features: - Created getTimeDifference which will set post date duration since posted - Rendered the image and video for the post - Removed the description

--- client/web/src/components/post/index.tsx
+++ client/web/src/components/post/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import PostType from "@/types/post";
+import HeartIcon from "@images/heart_icon.svg";
+import CommentIcon from "@images/comment_icon.svg";
+import BookmarkIcon from "@images/bookmark_icon.svg";
+import { serverURL } from "@/services/request";
+
 import "./post.css";
 
 const Post: React.FC<PostType> = ({
     uploader,
     profile_pic,
-    created_at,
+    updatedAt,
     title,
     ingredients,
     instructions,
@@ -14,48 +19,104 @@ const Post: React.FC<PostType> = ({
     saves,
     media,
 }) => {
+    const getTimeDifference = (updatedAt: string): string => {
+        const now = new Date();
+        const postDate = new Date(updatedAt);
+
+        const timeDifferenceInSeconds = Math.floor(
+            (now.getTime() - postDate.getTime()) / 1000
+        );
+
+        if (timeDifferenceInSeconds <= 60) {
+            return "just now";
+        } else if (timeDifferenceInSeconds < 3600) {
+            const minutes = Math.floor(timeDifferenceInSeconds / 60);
+            return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+        } else if (timeDifferenceInSeconds < 86400) {
+            const hours = Math.floor(timeDifferenceInSeconds / 3600);
+            return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+        } else if (timeDifferenceInSeconds < 2592000) {
+            const days = Math.floor(timeDifferenceInSeconds / 86400);
+            return `${days} ${days === 1 ? "day" : "days"} ago`;
+        } else if (timeDifferenceInSeconds < 31536000) {
+            const months = Math.floor(timeDifferenceInSeconds / 2592000);
+            return `${months} ${months === 1 ? "month" : "months"} ago`;
+        } else {
+            const years = Math.floor(timeDifferenceInSeconds / 31536000);
+            return `${years} ${years === 1 ? "year" : "years"} ago`;
+        }
+    };
+
+    const isImage = (mediaItem: string): boolean => {
+        const imageExtensions = ["jpg", "jpeg", "png", "gif"];
+        const fileExtension = mediaItem.split(".").pop();
+        if (fileExtension)
+            return imageExtensions.includes(fileExtension.toLowerCase());
+        return false
+    };
+
     return (
         <div className="post flex flex-column gap-5">
             <div className="post-header flex align-center gap-5">
                 <img
-                    src={profile_pic}
+                    src={`${serverURL}uploads/images/${profile_pic}`}
                     alt={`${uploader}'s profile`}
                     className="profile-pic"
                 />
                 <div className="uploader-info">
                     <p>{uploader}</p>
-                    <p>{created_at}</p>
+                    <p>{getTimeDifference(updatedAt)}</p>
                 </div>
             </div>
             <div className="post-content">
                 <h2>{title}</h2>
                 <div className="media-container">
                     {media &&
-                        media.map((image, index) => (
-                            <img
-                                key={index}
-                                src={image}
-                                alt={`Post media ${index}`}
-                                className="post-media"
-                            />
+                        media.map((mediaItem, index) => (
+                            <div key={mediaItem + index}>
+                                {isImage(mediaItem) ? (
+                                    <img
+                                        src={`${serverURL}${mediaItem}`}
+                                        alt={`Recipe Image: ${title}`}
+                                        className="post-media"
+                                    />
+                                ) : (
+                                    <video controls className="post-media">
+                                        <source
+                                            src={`${serverURL}${mediaItem}`}
+                                            type="video/mp4"
+                                        />
+                                        Your browser does not support the video
+                                        tag.
+                                    </video>
+                                )}
+                            </div>
                         ))}
                 </div>
                 <div className="post-description flex align-center justify-between">
                     <div className="flex flex-column align-center gap-5">
-                        {
-                            ingredients ? <p>{ingredients}</p> : <>
-                            <p>Ingredients not posted?</p>
-                            <button className="generate-btn">Generate it!</button>
+                        {ingredients ? (
+                            <p>{ingredients}</p>
+                        ) : (
+                            <>
+                                <p>Ingredients not posted?</p>
+                                <button className="generate-btn">
+                                    Generate it!
+                                </button>
                             </>
-                        }
+                        )}
                     </div>
                     <div className="flex flex-column align-center gap-5">
-                    {
-                            instructions ? <p>{instructions}</p> : <>
-                            <p>Instructions not posted?</p>
-                            <button className="generate-btn">Generate it!</button>
+                        {instructions ? (
+                            <p>{instructions}</p>
+                        ) : (
+                            <>
+                                <p>Instructions not posted?</p>
+                                <button className="generate-btn">
+                                    Generate it!
+                                </button>
                             </>
-                        }
+                        )}
                     </div>
                 </div>
             </div>
@@ -63,16 +124,13 @@ const Post: React.FC<PostType> = ({
                 <div className="post-actions-wrapper flex align-center justify-between">
                     <div className="post-actions flex gap-5">
                         <button className="flex gap-3 align-center">
-                            <img src={"src/assets/images/heart_icon.svg"} />{" "}
-                            {likes}
+                            <img src={HeartIcon} /> {likes.length}
                         </button>
                         <button className="flex gap-3 align-center">
-                            <img src={"src/assets/images/comment_icon.svg"} />{" "}
-                            {comments.length}
+                            <img src={CommentIcon} /> {comments.length}
                         </button>
                         <button className="flex gap-3 align-center">
-                            <img src={"src/assets/images/bookmark_icon.svg"} />{" "}
-                            {saves}
+                            <img src={BookmarkIcon} /> {saves}
                         </button>
                     </div>
                     <div className="post-add-comment">
@@ -90,11 +148,13 @@ const Post: React.FC<PostType> = ({
                             <div key={index} className="comment flex gap-4">
                                 <img
                                     src={comment.profile_pic}
-                                    alt={`${comment.username}'s profile`}
+                                    alt={`${comment.firstname}'s profile`}
                                     className="comment-pic"
                                 />
                                 <div className="comment-info flex flex-column gap-2">
-                                    <p>{comment.username}</p>
+                                    <p>
+                                        {comment.firstname} {comment.lastname}
+                                    </p>
                                     <p>{comment.comment} </p>
                                 </div>
                             </div>
