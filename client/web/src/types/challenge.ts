567d936012fc8a4bb906cf97390ed0add301215a
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-23 20:13:42 +0200

    [front-end] feat(type): Made some minor modifications on challenges type

--- client/web/src/types/challenge.ts
+++ client/web/src/types/challenge.ts
@@ -1,10 +1,10 @@
 import UserType from "./user"
 
 type Challenge = {
-  _id: string | null,
-  title: string | null,
-  description: string | null,
-  challengeImg: string | null,
+  _id: string,
+  title: string,
+  description: string,
+  challengeImg: string,
   challenger: UserType | null
 }
 
