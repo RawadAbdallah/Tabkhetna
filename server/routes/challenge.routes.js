12e90c4c303be89f576ddca0d881dc1e6ac3965a
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-23 20:27:17 +0200

    [back-end] feat(route): Imported needed functions from challenge controller

--- server/routes/challenge.routes.js
+++ server/routes/challenge.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const { getChallenges, createChallenge, participateChallenge } = require("../controllers/challenge.controller");
+const {getChallenges,createChallenge, participateChallenge} = require('../controllers/challenge.controller')
 const router = express.Router()
-
 router.get('/', getChallenges)
 router.post('/new', createChallenge)
 router.post('/participate/:challengeId', participateChallenge)
