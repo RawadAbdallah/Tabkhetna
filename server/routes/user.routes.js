ec61972080d7f55d571b3755ee71443a33d52756
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-24 04:19:29 +0200

    [back-end] feat(route): Added additional routes on the user.routes file

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,6 +7,7 @@ const {
     getCurrentUserProfile,
     getOtherUserProfile,
     getTopCookmates,
+    getProfileBasicInfo
 } = require("../controllers/user.controller");
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.post("/cookmates/accept/:id", acceptCookmate);
 router.post("/cookmates/reject/:id", rejectCookmate);
 router.get("/cookmates", getCookmates);
 router.get('/cookmates/top/:id', getTopCookmates)
-router.get("/profile", getCurrentUserProfile);
 router.get("/profile/:userId", getOtherUserProfile);
+router.get('/profile/basicInfo/:id', getProfileBasicInfo)
 
 module.exports = router;
