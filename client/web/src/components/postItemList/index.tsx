1034c1bd332421a61b4de28cdd2008983a8d8cfb
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-24 05:13:22 +0200

    [front-end] feat(component): Created postItemList component

---
+++ client/web/src/components/postItemList/index.tsx
@@ -0,0 +1,20 @@
+import './postItemList.css'
+
+interface ItemListProps {
+  inputString: string;
+}
+
+
+const PostItemList: React.FC<ItemListProps> = ({ inputString }) => {
+  const items = inputString.split(",").filter((item) => item.trim() !== "");
+
+  return (
+      <ul className="post-item-list">
+          {items.map((item, index) => (
+              <li key={index}>{`${item.trim()}`}</li>
+          ))}
+      </ul>
+  );
+};
+
+export default PostItemList
