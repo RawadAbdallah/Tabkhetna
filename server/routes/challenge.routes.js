e3cee1adcd29f5fc5aaac6462ca6a004b4b6de67
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-25 19:48:11 +0200

    [back-end] feat(route): Added createChallenge api route

--- server/routes/challenge.routes.js
+++ server/routes/challenge.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {getChallenges,createChallenge, participateChallenge} = require('../controllers/challenge.controller')
+const {getChallenges,createChallenge, participateChallenge} = require('../controllers/challenge.controller');
+const { upload } = require("../configs/multer.config");
 const router = express.Router()
 router.get('/', getChallenges)
-router.post('/new', createChallenge)
+router.post('/new', upload.single('challengeImg'), createChallenge)
 router.post('/participate/:challengeId', participateChallenge)
 
 module.exports = router;
