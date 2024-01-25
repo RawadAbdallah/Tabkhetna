9f745a536548bff276b7e70d363a57ef3b647898
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-26 00:53:15 +0200

    [back-end] refactor: Applied prettier

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -8,22 +8,21 @@ const {
     getLikes,
     getSaves,
     getAllSavedPosts,
-    getPosts
+    getPosts,
 } = require("../controllers/post.controller");
-const {mediaUpload} = require("../configs/multer.config");
+const { mediaUpload } = require("../configs/multer.config");
 
 const router = express.Router();
 
-
-
 // Define routes for the post controller
-router.post("/add", mediaUpload , addPost);
-router.post('/add-comment', addComment)
-router.post('/like/:id', addOrRemoveLike)
-router.post('/save/:id', saveUnsaveBookmark)
+router.post("/add", mediaUpload, addPost);
+router.post("/add-comment", addComment);
+router.post("/like/:id", addOrRemoveLike);
+router.post("/save/:id", saveUnsaveBookmark);
 router.get("/:id", getUserPosts);
-router.get('/like/get/:id', getLikes)
-router.get('/save/get/:id', getSaves)
-router.get('/save/all', getAllSavedPosts)
-router.get('/', getPosts)
+router.get("/like/get/:id", getLikes);
+router.get("/save/get/:id", getSaves);
+router.get("/save/all", getAllSavedPosts);
+router.get("/", getPosts);
+
 module.exports = router;
