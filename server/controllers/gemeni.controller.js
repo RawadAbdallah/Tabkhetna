3138ffdc12df0e711d52757aea89776c7daac85d
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-25 19:49:55 +0200

    [back-end] feat(controller): Updated the prompt for the Gemeni ai

--- server/controllers/gemeni.controller.js
+++ server/controllers/gemeni.controller.js
@@ -8,17 +8,12 @@ const getEngineeredPrompt = (title, listOfCurrentInfo, missing) => {
     if (missing === "instructions") {
         currentInfo = "ingredients";
     }
-    return `As a food chef, generate a basic set of instructions for a ${title} with the provided ${currentInfo}. Please provide clear and concise steps, avoiding additional phrases. Ensure that no comma is used within a sentence; use a comma only when separating each ${missing}. Omit numbers or dashes as items are separated by commas. Format the instructions as follows: (${missing}, ${missing}, ...). Keep it within 300 characters. I repeat commas are only written when item seperation is needed, for example: "Grab the milk, water and the eggs " should be without commas like this "Grab the milk water and the eggs".${currentInfo}: ${listOfCurrentInfo}`;
-};  
+    return `As a food chef, generate a basic set of instructions for a ${title} with the provided ${currentInfo}. Please provide clear and concise steps, avoiding additional phrases and don't generate any ${currentInfo}, just generate the ${missing}. Ensure that no comma is used within a sentence; use a comma only when separating each ${missing}. Omit numbers or dashes as items are separated by commas. Format the instructions as follows: (${missing}, ${missing}, ...). Keep it within 300 characters. I repeat commas are only written when item seperation is needed, for example: "Grab the milk, water and the eggs " should be without commas like this "Grab the milk water and the eggs". Here's the ${currentInfo} list: ${listOfCurrentInfo}`;
+};
 
 const generateRecipeMissingInfo = async (req, res) => {
     let { title, listOfCurrentInfo, missing } = req.body;
-    const prompt = getEngineeredPrompt(
-        title,
-        listOfCurrentInfo,
-        missing
-    );
-    console.log(prompt);
+    const prompt = getEngineeredPrompt(title, listOfCurrentInfo, missing);
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
         const result = await model.generateContent(prompt);
