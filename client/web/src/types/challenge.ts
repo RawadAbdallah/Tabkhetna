b322d17a3f9f6a7fa5099429c5d2aded40e749e5
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-26 00:05:01 +0200

    [front-end] feat(type): Added challeng_img & challenger fields to the Challenge Type

--- client/web/src/types/challenge.ts
+++ client/web/src/types/challenge.ts
@@ -4,8 +4,8 @@ type Challenge = {
   _id: string,
   title: string,
   description: string,
-  challengeImg: string,
-  challenger: UserType | null
+  challenge_img: string,
+  challenger: UserType | null,
 }
 
 export default Challenge
