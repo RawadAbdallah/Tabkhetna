316f67ceb673fcab33513d7a891e08d4907a2de0
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-25 19:51:57 +0200

    [back-end] feat(controller): Handled image uploading on challenge creation

--- server/controllers/challenge.controller.js
+++ server/controllers/challenge.controller.js
@@ -1,12 +1,20 @@
 const Challenge = require("../models/challenge.model");
-
+const User = require("../models/user.model");
 const getChallenges = async (req, res) => {
     try {
         const challenges = await Challenge.find();
-        if(!challenges){
-          return res.status(404).json({error: "No challenges at the moment"})
+        if (!challenges) {
+            return res
+                .status(404)
+                .json({ error: "No challenges at the moment" });
         }
-        return res.status(200).json({challenges})
+        for (const challenge of challenges) {
+            const user = await User.findById(challenge.challenger).select(
+                "-password"
+            );
+            challenge.challenger = user
+        }
+        return res.status(200).json({ challenges });
     } catch (e) {
         return res.status(500).json({ error: "Something went wrong" });
     }
@@ -15,6 +23,7 @@ const getChallenges = async (req, res) => {
 const createChallenge = async (req, res) => {
     const user = req.user;
     const { title, description } = req.body;
+
     try {
         if (!title) {
             return res
@@ -26,11 +35,21 @@ const createChallenge = async (req, res) => {
             return res.status(400).json({ error: "Description is reqiured" });
         }
 
-        const newChallenge = Challenge({
+        const newChallenge = new Challenge({
             title,
             description,
             challenger: user,
         });
+
+        // Handling the profile_pic
+        if (req.file) {
+            const image = req.file.filename;
+            newChallenge.challenge_img = image;
+        } else {
+            return res
+                .status(400)
+                .json({ error: "Challenge image is required." });
+        }
 
         await newChallenge.save();
         return res
@@ -46,7 +65,9 @@ const participateChallenge = async (req, res) => {
     const { challengeId } = req.params;
 
     try {
-        const challengeToParticipate = await Challenge.findOne({_id: challengeId})
+        const challengeToParticipate = await Challenge.findOne({
+            _id: challengeId,
+        });
         if (!challengeToParticipate) {
             return res.status(404).json({ error: "Challenge not found" });
         }
@@ -55,7 +76,7 @@ const participateChallenge = async (req, res) => {
             { _id: challengeId },
             { $push: { participants: user } }
         );
-        return res.status(200).json({message: "Participated successfully"})
+        return res.status(200).json({ message: "Participated successfully" });
     } catch (e) {
         console.log("Error in getting the challenge ", e);
         return res.status(500).json({ error: "Something went wrong" });
