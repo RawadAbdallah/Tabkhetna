acc85bdc06c84b0859e82d73733e1fa241f65257
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-28 10:35:40 +0200

    [back-end] feat(model): Added timestamps to challenge model

--- server/models/challenge.model.js
+++ server/models/challenge.model.js
@@ -1,38 +1,47 @@
 const mongoose = require("mongoose");
 
-const challengeSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    challenger: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    participants: [
-        {
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-            },
+const challengeSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
         },
-    ],
-    posts: [
-        {
+        description: {
+            type: String,
+            required: true,
+        },
+        challengeImg: {
+            type: String,
+            required: true,
+        },
+        challenger: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Post",
+            ref: "User",
+            required: true,
         },
-    ],
-    isActive: {
-        type: Boolean,
-        default: false,
+        participants: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
+                },
+            },
+        ],
+        posts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Post",
+            },
+        ],
+        isActive: {
+            type: Boolean,
+            default: false,
+        },
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Challenge = mongoose.model("Challenge", challengeSchema);
 
