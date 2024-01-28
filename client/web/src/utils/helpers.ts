6bf49d1e17c78293cd8642a75c08d1256337ef61
Rawad Abdallah <rawad0ne@gmail.com>
2024-01-29 00:00:33 +0200

    [front-end] feat(util): Modfied utilities

--- client/web/src/utils/helpers.ts
+++ client/web/src/utils/helpers.ts
@@ -206,3 +206,32 @@ const isValidSession = (user: string): boolean => {
     const token = parsedUser.token;
     return parsedUser && token;
 };
+
+// Get time difference for a post 
+
+export const getTimeDifference = (createdAt: string): string => {
+    const now = new Date();
+    const postDate = new Date(createdAt);
+    const timeDifferenceInSeconds = Math.floor(
+        (now.getTime() - postDate.getTime()) / 1000
+    );
+
+    if (timeDifferenceInSeconds <= 60) {
+        return "just now";
+    } else if (timeDifferenceInSeconds < 3600) {
+        const minutes = Math.floor(timeDifferenceInSeconds / 60);
+        return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+    } else if (timeDifferenceInSeconds < 86400) {
+        const hours = Math.floor(timeDifferenceInSeconds / 3600);
+        return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+    } else if (timeDifferenceInSeconds < 2592000) {
+        const days = Math.floor(timeDifferenceInSeconds / 86400);
+        return `${days} ${days === 1 ? "day" : "days"} ago`;
+    } else if (timeDifferenceInSeconds < 31536000) {
+        const months = Math.floor(timeDifferenceInSeconds / 2592000);
+        return `${months} ${months === 1 ? "month" : "months"} ago`;
+    } else {
+        const years = Math.floor(timeDifferenceInSeconds / 31536000);
+        return `${years} ${years === 1 ? "year" : "years"} ago`;
+    }
+};
